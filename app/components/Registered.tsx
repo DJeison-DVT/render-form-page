6678a086aa392289e5b05e6523b0a2db3817258d
Diego Villanueva Terrazas <diegovt.arbt@gmail.com>
2025-03-11 11:00:06 -0600

    add dashboard link button to Registered component

--- app/components/Registered.tsx
+++ app/components/Registered.tsx
@@ -1,4 +1,6 @@
+import { Button } from "@/components/ui/button";
 import CompanyImage from "./CompanyImage";
+import Link from "next/link";
 
 export default function Registered({ company }: { company: string }) {
 	return (
@@ -8,6 +10,9 @@ export default function Registered({ company }: { company: string }) {
 			</h2>
 			<CompanyImage company={company} size={400} />
 			<p>En breve nos pondremos en contacto contigo.</p>
+			<Button className="mt-4">
+				<Link href="/renders/dashboard">Ir al dashboard</Link>
+			</Button>
 		</div>
 	);
 }
