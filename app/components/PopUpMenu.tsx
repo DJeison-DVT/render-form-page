da6e0a12bba098a3648c94d29715e118b4e65a1e
Diego Villanueva Terrazas <diegovt.arbt@gmail.com>
2025-05-06 20:05:40 -0600

    Add historial to Provider options

--- app/components/PopUpMenu.tsx
+++ app/components/PopUpMenu.tsx
@@ -43,13 +43,11 @@ export default async function PopUpMenu() {
 						<Button variant="link">
 							<Link href="/renders/dashboard">Dashboard</Link>
 						</Button>
-						{role !== Role.PROVIDER && (
-							<Button variant="link">
-								<Link href="/renders/dashboard/history">
-									Historial
-								</Link>
-							</Button>
-						)}
+						<Button variant="link">
+							<Link href="/renders/dashboard/history">
+								Historial
+							</Link>
+						</Button>
 						{adminRoles.includes(role as Role) && (
 							<Button variant="link">
 								<Link href="/renders/registration">
